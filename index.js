298758ba16587e513cf3dbfbfdfb2551b0b15c6f
abdullah <abdullahiqbal599@gmail.com>
2024-01-15 21:32:55 +0500

    Upload posts

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import multer from "multer";
 
 const app = express();
 const port = 5000;
 
 // middleware we use
 
+// first 
 app.use((req, res , next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
@@ -20,11 +22,30 @@ app.use((req, res , next) => {
 app.use(express.json());
 app.use(cors({
     origin: "http://localhost:3000"
-}));    
+}));
+
+// second
 app.use(cookieParser());
 
+// third
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../social-media/public/upload')
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, Date.now() + file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
+  app.post('/api/upload', upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename)
+  })
 
+// routes   
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getPosts } from '../controller/post.js';
+import { getPosts, addPost } from '../controller/post.js';
 
 const router = express.Router();
 
 
 
 router.get('/', getPosts)
+router.post('/', addPost)
 
 export default router;
 
